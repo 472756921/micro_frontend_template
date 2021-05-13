5f4ce102450bb2edceea0ff20a27435df751cafc
BensonChen <472756921@qq.com>
2021-05-13 14:14:37 +0800

    refactor(add eslint): eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import styles from './index.less';
 
 export default function IndexPage() {
+  const getName = (name) => {};
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
